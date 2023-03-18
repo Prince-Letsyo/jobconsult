05a29a0859ea2e9adeaf59703a3f09dd1606bd9d
Prince-Letsyo <princeletsyo1596@gmail.com>
2023-03-18 19:56:57 +0000

    setting up frontend 02

---
+++ frontend/jcproject/src/store/features/authSlice/index.js
@@ -0,0 +1,79 @@
+import { apiSlice } from "../api";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    registerNewUser: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/register/",
+        method: "POST",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/login/",
+        method: "POST",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+    logoutUser: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/logout/",
+        method: "POST",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+    passwordResetComplete: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/password-reset-complete/",
+        method: "PATCH",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+    requestPasswordReset: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/request-password-reset/",
+        method: "POST",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+    createNewAccessToken: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/token/refresh/",
+        method: "POST",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+    verifyEmail: builder.mutation({
+      query: (initialUser) => ({
+        url: "/users/verify-email/",
+        method: "POST",
+        body: {
+          ...initialUser,
+        },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useRegisterNewUserMutation,
+  useLoginUserMutation,
+  useLogoutUserMutation,
+  usePasswordResetCompleteMutation,
+  useRequestPasswordResetMutation,
+  useCreateNewAccessTokenMutation,
+  useVerifyEmailMutation,
+} = authApiSlice;
---
+++ frontend/jcproject/src/store/features/userSlice/index.js
@@ -0,0 +1,66 @@
+import { createEntityAdapter, createSelector } from "@reduxjs/toolkit";
+import { apiSlice } from "../api";
+
+const userAdapter = createEntityAdapter();
+
+const initialState = userAdapter.getInitialState();
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "/users/",
+      transformResponse: (responseData) => {
+        return userAdapter.setAll(initialState, responseData);
+      },
+      providesTags: (result, error, arg) => [
+        { type: "User", id: "LIST" },
+        ...result.ids.map((id) => ({ type: "User", id })),
+      ],
+    }),
+    getUserByUserId: builder.query({
+      query: ( id ) => `/users/${id}/`,
+      providesTags: (result, error, arg) => [
+        ...result.ids.map((id) => ({ type: "User", id })),
+      ],
+    }),
+    changeUserInfo: builder.mutation({
+      query: (initialUser) => `/users/${initialUser.id}/`,
+      method: "PUT",
+      body: {
+        ...initialUser,
+      },
+    }),
+    mutateUserInfo: builder.mutation({
+      query: (initialUser) => `/users/${initialUser.id}/`,
+      method: "PATCH",
+      body: {
+        ...initialUser,
+      },
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => `/users/${id}/`,
+      method: "DELETE",
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useGetUserByUserIdQuery,
+  useMutateUserInfoMutation,
+  useChangeUserInfoMutation,
+  useDeleteUserMutation,
+} = usersApiSlice;
+
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
+
+export const selectUsersData = createSelector(
+  selectUsersResult,
+  (userResult) => userResult.data
+);
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+  selectIds: selectUserIds,
+} = userAdapter.getSelectors((state) => selectUsersData(state) ?? initialState);

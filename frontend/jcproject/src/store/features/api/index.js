7d1d11f7acb862d9d821685bc74904e2e9988846
Prince-Letsyo <princeletsyo1596@gmail.com>
2023-03-30 07:25:28 +0000

    setting up frontend 13

--- frontend/jcproject/src/store/features/api/index.js
+++ frontend/jcproject/src/store/features/api/index.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pletsyo.pythonanywhere.com/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api" }),
   tagTypes: [
     "User",
     "JobSeeker",
